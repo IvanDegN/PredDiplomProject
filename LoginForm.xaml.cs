998da8d7181bb9d8313ae347b335019dc467eb3d
IvanDegN <mauzer.bruno@yandex.ru>
2022-05-03 18:49:18 +0300

    Добавьте файлы проекта.

---
+++ LoginForm.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PredDiplomProject
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginForm.xaml
+    /// </summary>
+    public partial class LoginForm : Window
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TbLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
